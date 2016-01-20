81be135b3d1d615f895d2895972dfd3651724493
p-pujitha <p-pujitha@missionrnd.com>
2016-01-20 12:35:05 +0530

    Submit Commit

--- src/getFrequencyofWord.cpp
+++ src/getFrequencyofWord.cpp
@@ -12,50 +12,78 @@ Note: Dont modify original str or word,Just return count ,Spaces can also be par
 
 #include <stdlib.h>
 
+int string_length(char *str);
+
+/*void recur_count(char *str, char *word, int i, int j, int *count);
+bool substring_compare(char *str, char *word, int i, int j);
+*/
+//Not a recursive function
+
+int string_length(char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0'; i++);
+	return i;
+}
+
 int count_word_in_str_way_1(char *str, char *word){
 
-	int j = 0, flag = 0, count = 0;
-	for (int i = 0; str[i] != '\0';)
+	int i, j, word_len = string_length(word);
+	int str_len = string_length(str);
+	int count = 0, flag;
+	for (i = 0; i <= str_len - word_len; i++)
 	{
-		if (str[i] == word[j])
+		flag = 1;
+		for (j = i; j < i + word_len; j++)
 		{
+			if (str[j] != word[j - i])
+			{
+				flag = 0;
+				break;
+			}
+		}
+		if (flag == 1)
+			count++;
+	}
+	return count;
+}
+
+/*int count_word_int_str_way_2_recursion(char *str, char *word)
+{
+
+		int count = 0;
+		recur_count(str, word, 0, string_length(word) - 1, &count);
+		return count;
+		
+}
+
+void recur_count(char *str, char *word, int i, int j, int *count)
+{
+		
+		if (i < string_length(str) - (j - i) + 1)
+		{
+			if (substring_compare(str, word, i, j))
+				*count = *count + 1;
 			i++;
 			j++;
-			while (word[j] != '\0')
-			{
-				if (str[i] == word[j])
-				{
-					flag = 1;
-					i++;
-					j++;
-				}
-				else
-				{
-					flag = 0;
-					break;
-				}
-			}
-			if (flag == 1)
-			{
-				j = 0;
-				count++;
-				flag = 0;
-			}
-
+			recur_count(str, word, i, j, count);
 		}
+}
 
-		else
+bool substring_compare(char *str, char *word, int i, int j)
+{
+		int k;
+		bool flag = true;
+		for (k = i; k <= j; k++)
 		{
-
-			i++;
+			if (str[k] != word[k - i])
+			{
+				flag = false;
+				break;
+			}
 		}
-	}
-
-	return count;
-	return 0;
+		return flag;
 }
+		*/
 
-int count_word_int_str_way_2_recursion(char *str, char *word){
-	return 0;
-}
-
+	
